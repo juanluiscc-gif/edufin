874021a3e3e32e5f9ddc54782f9f98e7625ab409
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2025-12-26 12:38:12 -0600

    Fix locale type error in page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import LandingPage from '@/components/LandingPage';
+import { Locale } from '@/i18n';
 
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
-  return <LandingPage locale={locale} />;
+  return <LandingPage locale={locale as Locale} />;
 }
