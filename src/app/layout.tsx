a0599e34a4882a9a94d4bc5fdc55b5872f0ad23c
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2025-12-28 16:41:56 -0600

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'EduFin',
+  description: 'Financial Education',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
