10dc361178e80498062618cce906428ff838aacb
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2025-12-28 17:42:59 -0600

    Create page.tsx

---
+++ src/locales/en/page.tsx
@@ -0,0 +1,6 @@
+import LandingPage from '@/components/LandingPage';
+import { Locale } from '@/i18n/request';
+
+export default function EnglishPage() {
+  return <LandingPage locale={'en' as Locale} />;
+}
