a467f1ada9907cd4c655367d4498a8987b1bf6b5
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2025-12-29 14:24:48 -0600

    Delete src/locales/en/page.tsx

--- src/locales/en/page.tsx
+++
@@ -1,6 +0,0 @@
-import LandingPage from '@/components/LandingPage';
-import { Locale } from '@/i18n/request';
-
-export default function EnglishPage() {
-  return <LandingPage locale={'en' as Locale} />;
-}
