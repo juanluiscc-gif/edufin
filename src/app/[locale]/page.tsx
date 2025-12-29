5c86cf30ca8be8de986c2904dca0f5931ef43a79
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2025-12-29 17:34:41 -0600

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
-import { locales } from '@/i18n/request';
+import LandingPage from '@/components/LandingPage';
+import { Locale, locales } from '@/i18n/request';
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -6,10 +7,5 @@ export function generateStaticParams() {
 
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
-  return (
-    <div>
-      <h1>LOCALE PAGE WORKS</h1>
-      <p>Locale: {locale}</p>
-    </div>
-  );
+  return <LandingPage locale={locale as Locale} />;
 }
