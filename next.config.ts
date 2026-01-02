13594c063ce25398704673221b14361b06cf2ef2
Juan Luis Cortés Cervantes <juanluiscortescervantes@gmail.com>
2026-01-02 15:15:48 -0600

    Update next.config.ts
    ACTUALIZACIÓN.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude Prisma from client-side bundle
+      config.externals = [...(config.externals || []), '@prisma/client'];
+    }
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
